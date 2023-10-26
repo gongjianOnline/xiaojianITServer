ee0791e62825b30bee4179b4c9db4ba058058b88
gongjian <15031617367@163.com>
2023-10-26 21:10:43 +0800

    解决跨域

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ const users = require('./routes/users')
 // error handler
 onerror(app)
 
+/**解决跨域 */
+app.use(async (ctx, next)=> {
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  if (ctx.method == 'OPTIONS') {
+    ctx.body = 200; 
+  } else {
+    await next();
+  }
+});
+
 // middlewares
 app.use(bodyparser({
   enableTypes:['json', 'form', 'text']
